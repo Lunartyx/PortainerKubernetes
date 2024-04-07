052c7de7d73538b07e522bcfb3dd7524549438c4
Lunartyx <dominik@urbanetz.li>
2024-04-07 15:49:18 +0200

    update entrypoints

--- container-images/docker/api/entrypoint.sh
+++ container-images/docker/api/entrypoint.sh
@@ -1,16 +1,12 @@
-# Clone repository and copy files
+#!/bin/bash
+
 git clone https://github.com/Lunartyx/PortainerKubernetes.git /tmp/repository && \
     cp -r /tmp/repository/web/frontend/* /api
 
-# Clean up
 rm -rf /tmp/repository
 
-# Change directory
 cd /api
 
-# Install dependencies
 npm install
 
-# Start the app
 npm run start
-
--- container-images/docker/frontend/entrypoint.sh
+++ container-images/docker/frontend/entrypoint.sh
@@ -1,15 +1,11 @@
-# Clone repository and copy files
+#!/bin/bash
 git clone https://github.com/Lunartyx/PortainerKubernetes.git /tmp/repository && \
     cp -r /tmp/repository/web/frontend/* /app
 
-# Clean up
 rm -rf /tmp/repository
 
-# Change directory
 cd /app
 
-# Install dependencies
 npm install
 
-# Start the app
 npm run start
